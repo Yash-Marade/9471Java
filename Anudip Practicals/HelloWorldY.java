1ef9c94785e4dd82ae1e2145eedbb7f72bcbf440
Yash-Marade <95099910+Yash-Marade@users.noreply.github.com>
2021-12-05 00:38:18 +0530

    Add files via upload

---
+++ Anudip Practicals/HelloWorldY.java
@@ -0,0 +1,7 @@
+public class HelloWorldY
+{
+	public static void main(String[] args)
+  {
+		System.out.println("Hello Yash");
+	}
+}
---
+++ Anudip Practicals/RelationalOpeY.java
@@ -0,0 +1,23 @@
+public class RelationalOpeY
+{
+  public static void main(String args[])
+  {
+  int a=50;
+  int b=50;
+  if(a==b)
+{
+System.out.println("a and B are equal");
+}
+else {
+System.out.println("a and are not equal");
+}
+if(a!=b)
+{
+System.out.println("a and B are not equal");
+}
+else {
+
+System.out.println("a and b are equal");
+}
+}
+} 
