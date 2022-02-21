e745c8ecac702e58b9b88a64a9895d9bf87cd518
Yash M <95099910+Yash-Marade@users.noreply.github.com>
2022-02-21 22:09:28 +0530

    Add files via upload

---
+++ Spring_Projects/S_1/src/S_1/Test.java
@@ -0,0 +1,15 @@
+package S_1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+public static void main(String args[]) {
+	ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+    
+    Student student=(Student)ac.getBean("studentbean");  
+		    student.displayInfo(); 
+		    ((ClassPathXmlApplicationContext) ac).close();
+		    
+}
+}
